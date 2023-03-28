dcc5539d9fbac3ec06bbaf09b68d8800bb97c842
jahongir1507 <ishankulovjahongir1507@gmail.com>
2023-03-29 01:55:26 +0500

    BROKERS: CRUD -> "Language"

--- CashOverflow.Ddemo/Brokers/Storages/IStorageBroker.Location.cs
+++ CashOverflow.Ddemo/Brokers/Storages/IStorageBroker.Location.cs
@@ -10,7 +10,7 @@ namespace CashOverflow.Ddemo.Brokers.Storages
     public partial interface IStorageBroker
     {
         ValueTask<Location> InsertLocationAsync(Location location);
-        IQueryable<Location> SelectAllLocationsAsync();
+        IQueryable<Location> SelectAllLocations();
         ValueTask<Location> SelectLocationByIdAsync(Guid locationId);
         ValueTask<Location> UpdateLocationAsync(Location location);
         ValueTask<Location> DeleteLocationAsync(Location location);
--- CashOverflow.Ddemo/Brokers/Storages/StorageBroker.Language.cs
+++ CashOverflow.Ddemo/Brokers/Storages/StorageBroker.Language.cs
@@ -4,6 +4,7 @@
 // --------------------------------------------------------
 
 using CashOverflow.Ddemo.Models.Languages;
+using CashOverflow.Ddemo.Models.Locations;
 using Microsoft.EntityFrameworkCore;
 
 namespace CashOverflow.Ddemo.Brokers.Storages
@@ -11,5 +12,20 @@ namespace CashOverflow.Ddemo.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<Language> Language { get; set; }
+
+        public async ValueTask<Language> InsertLanguageAsync(Language language) =>
+            await InsertAsync(language);
+
+        public async ValueTask<Language> SelectLanguageByIdAsync(Guid languageId) =>
+            await SelectAsync<Language>(languageId);
+
+        public IQueryable<Language> SelectAllLanguages() =>
+            SelectAll<Language>();
+
+        public async ValueTask<Language> UpdateLanguageAsync(Language language) =>
+            await UpdateAsync<Language>(language);
+
+        public async ValueTask<Language> DeleteLanguageAsync(Language language) =>
+            await DeleteAsync<Language>(language);
     }
 }
