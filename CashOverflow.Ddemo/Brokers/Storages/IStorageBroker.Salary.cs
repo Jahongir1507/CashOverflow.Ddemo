392c4291b3c6c60a2da840d27c32d76b3d2f55bd
jahongir1507 <ishankulovjahongir1507@gmail.com>
2023-03-29 02:08:18 +0500

    BROKERS: CRUD -> "Salary"

---
+++ CashOverflow.Ddemo/Brokers/Storages/IStorageBroker.Salary.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineer
+// Developed by CashOverflow Jahongir
+// --------------------------------------------------------
+
+using System.Security.Principal;
+using CashOverflow.Ddemo.Models.Salaries;
+
+namespace CashOverflow.Ddemo.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Salary> InsertSalaryAsync(Salary salary);
+        IQueryable<Salary> SelectAllSalaries();
+        ValueTask<Salary> SelectSalaryByIdAsync(Guid salaryId);
+    }
+}
