10ed0d34c56e42f29c0b506d4c775dc6cf8c19bc
jahongir1507 <ishankulovjahongir1507@gmail.com>
2023-03-28 22:34:15 +0500

    DATA: Model & Migrate "Review"

---
+++ CashOverflow.Ddemo/Migrations/20230328172957_CreatedReviewTable.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineer
+// Developed by CashOverflow Jahongir
+// --------------------------------------------------------
+
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CashOverflow.Ddemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedReviewTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Review",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Stars = table.Column<int>(type: "int", nullable: false),
+                    Thoughts = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Review", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Review");
+        }
+    }
+}
---
+++ CashOverflow.Ddemo/Models/Reviews/Review.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineer
+// Developed by CashOverflow Jahongir
+// --------------------------------------------------------
+
+namespace CashOverflow.Ddemo.Models.Reviews
+{
+    public class Review
+    {
+        public Guid Id { get; set; }
+        public string CompanyName { get; set; }
+        public int Stars { get; set; }
+        public string Thoughts { get; set; }
+    }
+}
