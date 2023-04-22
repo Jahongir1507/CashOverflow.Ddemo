a07754d083fefdb8af2ed22a6b25c55251e663c1
Jahongir Ishankulov <ishankulovjahongir1507@gmail.com>
2023-04-22 13:23:55 +0500

    BROKERS: Add Logging

---
+++ CashOverflow.Ddemo/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineer
+// Developed by CashOverflow Jahongir
+// --------------------------------------------------------
+
+namespace CashOverflow.Ddemo.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private readonly ILogger<LoggingBroker> logger;
+
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
+            this.logger = logger;
+
+        public void LogError(Exception exception) =>
+            this.logger.LogError(exception.Message, exception);
+
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception.Message, exception);
+    }
+}
